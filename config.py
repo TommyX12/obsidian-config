033c3940e2996b356b94333d5406cd1737488abd
TommyX12 <tommyx058@gmail.com>
2021-11-15 12:18:35 -0800

    iinitial

---
+++ config.py
@@ -0,0 +1,12 @@
+FILES_TO_COPY = [
+    '.obsidian/.gitignore',
+    '.obsidian/app.json',
+    '.obsidian/appearance.json',
+    '.obsidian/community-plugins.json',
+    '.obsidian/core-plugins.json',
+    '.obsidian/hotkeys.json',
+    '.obsidian/page-preview.json',
+    '.obsidian/plugins',
+    '.obsidian/snippets',
+    '.obsidian/themes',
+]
---
+++ sync_to.py
@@ -0,0 +1,20 @@
+import os
+import shutil
+import distutils
+from distutils import dir_util
+from config import FILES_TO_COPY
+
+REPO_DIR = os.path.dirname(os.path.abspath(__file__))
+CUR_DIR = os.path.abspath(os.getcwd())
+
+assert os.path.isdir(os.path.join(CUR_DIR, '.obsidian')), 'You must be in an Obsidian vault'
+
+for f in FILES_TO_COPY:
+    source = os.path.join(CUR_DIR, f)
+    target = os.path.join(REPO_DIR, f)
+    os.makedirs(os.path.dirname(target), exist_ok=True)
+    if os.path.isfile(f):
+        shutil.copy2(source, target)
+
+    elif os.path.isdir(f):
+        dir_util.copy_tree(source, target)
